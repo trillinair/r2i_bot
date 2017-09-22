bb611f159f76720364b3c8f914d9031379666428
Ilia Choly <ilia.choly@gmail.com>
2017-09-22 17:59:43 -0400

    reuse font

--- main.go
+++ main.go
@@ -4,11 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/fogleman/gg"
+	"github.com/golang/freetype/truetype"
 	"image"
 	"image/color"
 	"io"
+	"io/ioutil"
 	"os"
 )
+
+var font *truetype.Font
+
+func init() {
+	data, err := ioutil.ReadFile("/Library/Fonts/Arial.ttf")
+	if err != nil {
+		panic(err)
+	}
+	font, err = truetype.Parse(data)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	titles, err := ReadTitles("titles.txt")
@@ -46,22 +61,28 @@ func ReadTitles(filename string) ([]string, error) {
 	return lines, nil
 }
 
-func LoadBestFont(dc *gg.Context, s string, lineHeight, h, w float64) error {
+func SetFontFace(dc *gg.Context, points float64) {
+	dc.SetFontFace(
+		truetype.NewFace(font, &truetype.Options{
+			Size: points,
+		}),
+	)
+}
+
+func SetBestFontFace(dc *gg.Context, s string, lineHeight, h, w float64) {
 	points := float64(40)
 	for {
-		if err := dc.LoadFontFace("/Library/Fonts/Arial.ttf", points); err != nil {
-			return err
-		}
+		SetFontFace(dc, points)
 		_, fontHeight := dc.MeasureString(s)
 		nLines := float64(len(dc.WordWrap(s, w)))
 		wrappedHeight := fontHeight * lineHeight * nLines
-
 		if wrappedHeight < h {
+			fmt.Printf("difference", wrappedHeight-h)
 			break
 		}
 		points--
 	}
-	return dc.LoadFontFace("/Library/Fonts/Arial.ttf", points)
+	SetFontFace(dc, points)
 }
 
 func MakeImage(text string) (image.Image, error) {
@@ -77,9 +98,7 @@ func MakeImage(text string) (image.Image, error) {
 	dc.SetLineWidth(8)
 	dc.Stroke()
 	dc.Height()
-	if err := LoadBestFont(dc, text, LH, H-P-P-P-P, W-P-P-P-P); err != nil {
-		return nil, err
-	}
+	SetBestFontFace(dc, text, LH, H-P-P-P-P, W-P-P-P-P)
 	dc.DrawStringWrapped(text, P+P, P+P, 0, 0, W-P-P-P-P, LH, gg.AlignCenter)
 	return dc.Image(), nil
 }
