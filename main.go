0ae2057a6efa66433db858b4db084e5ab2547e16
Ilia Choly <ilia.choly@gmail.com>
2017-09-22 20:08:09 -0400

    remove old testing code

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"github.com/fogleman/gg"
-	"io"
 	"log"
 	"math/rand"
-	"os"
 	"time"
 )
 
@@ -39,40 +36,4 @@ func main() {
 			break
 		}
 	}
-}
-
-func GenerateImages() {
-	titles, err := ReadTitles("titles.txt")
-	if err != nil {
-		panic(err)
-	}
-	for i, title := range titles {
-		im, err := MakeImage(title)
-		if err != nil {
-			panic(err)
-		}
-		fname := fmt.Sprintf("out%d.png", i)
-		gg.SavePNG(fname, im)
-	}
-}
-
-func ReadTitles(filename string) ([]string, error) {
-	f, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	r := bufio.NewReader(f)
-	var lines []string
-	for {
-		line, err := r.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return nil, err
-		}
-		lines = append(lines, line)
-	}
-	return lines, nil
 }
